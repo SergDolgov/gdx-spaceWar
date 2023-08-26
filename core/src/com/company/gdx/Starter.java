ef4a4e329551de47237a9b1ec38fc310fe087fbe
Sergey Dolgov <saddolgov@gmail.com>
2023-08-26 12:02:11 +0300

    update

--- core/src/com/company/gdx/Starter.java
+++ core/src/com/company/gdx/Starter.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class SpaceWarGame extends Game {
+public class Starter extends Game {
 	private SpriteBatch batch;
 
 	@Override
--- html/src/com/company/gdx/client/HtmlLauncher.java
+++ html/src/com/company/gdx/client/HtmlLauncher.java
@@ -3,7 +3,7 @@ package com.company.gdx.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
-import com.company.gdx.SpaceWarGame;
+import com.company.gdx.Starter;
 
 public class HtmlLauncher extends GwtApplication {
 
@@ -17,6 +17,6 @@ public class HtmlLauncher extends GwtApplication {
 
         @Override
         public ApplicationListener createApplicationListener () {
-                return new SpaceWarGame();
+                return new Starter();
         }
 }
